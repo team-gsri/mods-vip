acae2bfa1a6904e4325e2d018446e77d5bdc355f
PhileasLX <x@x.x>
2022-10-24 03:47:57 +0200

    feat: Add CBA setting to allow or not the player to have a weapon in hands in order to equip gear

--- addons/gsri_vip/config.cpp
+++ addons/gsri_vip/config.cpp
@@ -7,15 +7,23 @@ class CfgPatches {
     };
 };
 
+class Extended_PreInit_EventHandlers {
+  class gsri_vip {
+    init = "call GSRI_fnc_vipProtectionSettings";
+  };
+};
+
 class CfgFunctions {
 	class GSRI {
 		class protection{
 			file="fr\gsri\vip\functions\protection";
+            class vipCanEquipGear {};
 			class vipCheckForGear {};
 			class vipEquipGear {};
 			class vipEquipGearFailure {};
 			class vipEquipGearSuccess {};
 			class vipProcessGearInfos {};
+            class vipProtectionSettings {};
 			class vipTryEquipGear {};
 		};
 		class init{
---
+++ addons/gsri_vip/functions/protection/fn_vipProtectionSettings.sqf
@@ -0,0 +1,10 @@
+[
+  "gsri_vip_weaponAllowed",
+  "CHECKBOX",
+  [localize "STR_GSRI_weapon_allowed", localize "STR_GSRI_weapon_allowed_tooltip"],
+  ["GSRI VIP", "Protection"],
+  true,
+  1,
+  {},
+  false
+] call CBA_fnc_addSetting;
